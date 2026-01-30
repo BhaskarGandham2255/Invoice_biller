ac683457c54d994ec625d91d17a16f9184d345e0
bhask <bhask@gandham>
2026-01-30 21:08:08 +0530

    Initial commit: Spring Boot base project

---
+++ src/main/java/com/bn/InvoiceBillerApplication.java
@@ -0,0 +1,13 @@
+package com.bn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceBillerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceBillerApplication.class, args);
+	}
+
+}
