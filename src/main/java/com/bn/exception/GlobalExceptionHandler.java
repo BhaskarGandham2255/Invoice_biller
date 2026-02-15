f9d9f53576b8b12cd35665d9abaa8a3d9515990c
Durga venkata naga bhaskar Gandham <bhaskargandham2255@gmail.com>
2026-02-15 16:15:05 +0530

    fix(exception): update exception handling to use Model for error messages

--- src/main/java/com/bn/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bn/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.bn.exception;
 
-import java.util.Map;
-
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -9,8 +8,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler 
 {
 	 @ExceptionHandler(Exception.class)
-	    public String handleException(Exception ex,Map<String, Object> model) {
-	        model.put("errorMsg", ex.getMessage());
+	    public String handleException(Exception ex,Model model) {
+		 System.out.println("Exception caught:\n\n\n\n" + ex.getMessage());
+	        model.addAttribute("errorMsg", ex.getMessage());
 	        return "error";
 	    }
 }
