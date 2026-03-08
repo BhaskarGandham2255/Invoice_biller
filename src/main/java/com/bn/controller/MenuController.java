4beb38f607167a47589e24bc7ab9d5fe855c5cca
Bhaskargandham2255 <bhaskargandham2255@gmail.com>
2026-03-08 23:54:45 +0530

    fix(menu): replace @RequestMapping with @GetMapping for loadHomePage

--- src/main/java/com/bn/controller/MenuController.java
+++ src/main/java/com/bn/controller/MenuController.java
@@ -5,10 +5,11 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bn.entity.Item;
-import com.bn.service.MenuService;
+import com.bn.service.MenuService;          
 
 @Controller
 @RequestMapping("/menu")
@@ -17,10 +18,11 @@ public class MenuController {
 	@Autowired
 	private MenuService menuSer;
 	
-	@RequestMapping({"/","/home"})
+	@GetMapping({"/","/home"})
 	public String loadHomePage(Map<String, Object> model) {
 		List<Item> allItems = menuSer.getAllItems();
 		model.put("items", allItems);
 		return "index";
 	}
+	
 }
